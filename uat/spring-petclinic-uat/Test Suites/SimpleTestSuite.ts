ee08c58df7ecb038f54820050061fdd59baac809
Patrick Keller <patrick.keller.001@student.uni.lu>
2018-12-10 02:41:07 +0100

    Add Katalon test suite

---
+++ uat/spring-petclinic-uat/Test Suites/SimpleTestSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SimpleTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8800ee1d-a853-4cdd-92c5-8ea2853f8568</testSuiteGuid>
+   <testCaseLink>
+      <guid>0923cff2-2a88-4277-b0aa-85486d3f5190</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SearchTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
